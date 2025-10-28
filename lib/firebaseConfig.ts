6e4df5b69aef84a7c22fcfc286bbc65524138a1b
Abhishek <rambopranjal63@gmail.com>
2025-10-28 23:15:12 +0530

    Added ENV

--- lib/firebaseConfig.ts
+++ lib/firebaseConfig.ts
@@ -3,12 +3,12 @@ import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCndTPW58_58YG0V7S6MuEjby00oX56Tcw",
-  authDomain: "meet-scheduler-1fabf.firebaseapp.com",
-  projectId: "meet-scheduler-1fabf",
-  storageBucket: "meet-scheduler-1fabf.firebasestorage.app",
-  messagingSenderId: "923641075503",
-  appId: "1:923641075503:web:5032b30e0640cf547433da"
+  apiKey: process.env.EXPO_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.EXPO_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.EXPO_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.EXPO_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.EXPO_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.EXPO_PUBLIC_FIREBASE_APP_ID
 };
 
 // Initialize Firebase
